5ffab4c8eaa65f2e31c1bb2f75bec51dab684ec8
NewSulc <vsulc007@outlook.cz>
2023-05-24 18:53:27 +0200

    wrafetszhduiolpůú§¨ů§

--- Zaverecka/scripts/dropdown.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector('#hamburger');
-const dropdown = document.querySelector('#dropdownMenu');
-
-toggleBtn.onclick = function () {
-    dropdown.classList.toggle('open');
-    toggleBtn.classList.toggle('active');
-}
